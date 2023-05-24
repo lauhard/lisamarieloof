5e83c8b22c9654b2c04655a1bc75390f5f28706f
lauhard.andreas <lauhard.andreas@gmail.com>
2023-05-24 19:52:57 +0200

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,8 +13,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			edge: true,
-			split: false
+			// edge: true,
+			// split: false
 		})
 	}
 };
