a76e8958ee2a9754b810ed17f024b034610ec32f
lauhard.andreas <lauhard.andreas@gmail.com>
2023-06-12 18:08:46 +0200

    add services

--- src/routes/leistungen/+page.svelte
+++ src/routes/leistungen/+page.svelte
@@ -1,5 +1,235 @@
 <script lang="ts">
-
+    import rauchfrei from "$lib/images/rauchfrei.jpg";
+    import digitaldetox from "$lib/images/digitaldetox.jpg";
+    import pruefungsangst from "$lib/images/pruefungsangst.jpg";
+    import selbstwert from "$lib/images/selbstwert.jpg";
+    import tiefenentspannung from "$lib/images/tiefenentspannung.jpg";
 </script>
 
-    <h2>Meine Pakete</h2>
+<svelte:head>
+    <title>Startseite</title>
+    <link rel="preload" as="image" href={rauchfrei} />
+    <link rel="preload" as="image" href={digitaldetox} />
+    <link rel="preload" as="image" href={pruefungsangst} />
+    <link rel="preload" as="image" href={selbstwert} />
+    <link rel="preload" as="image" href={tiefenentspannung} />
+</svelte:head>
+
+<section class="section section-one">
+    <div class="content-wrapper">
+        <h2 class="decorator">Meine Pakete</h2>
+        <div class="wrapper-services">
+            <div class="service rauchfrei">
+                <h2>Raucherentwöhnung</h2>
+                <div class="service-image">
+                    <img src={rauchfrei} alt="rauchfrei" srcset=""   width="100%"
+                    height="300px"/>
+                </div>
+                <div class="service-text">
+                    <p>
+                        Zigaretten enthalten nicht wenige äußerst toxische
+                        Substanzen, wie Stickstoffoxide, Schwefeloxide und
+                        Ammoniak. Allein in Österreich sterben im Jahr ungefähr
+                        14.000 Menschen an tabakinduzierten Krankheiten, wobei
+                        das Rauchen zu dem größten vermeidbaren Risikofaktor für
+                        viele verschiedene Krankheiten zählt. Das und vieles
+                        mehr wissen Sie aber bereits, sonst würden Sie sich hier
+                        nicht über die Möglichkeit der Rauchentwöhnung mittels
+                        Hypnose informieren.
+                        <br />
+                        Ich würde mich freuen, Sie kennenzulernen und Sie bei Ihrem
+                        Vorhaben, mit dem Rauchen aufzuhören, zu unterstützen.
+                    </p>
+                </div>
+            </div>
+            <div class="service selbstwert">
+                <h2>Selbstwertsteigerung</h2>
+                <div class="service-image">
+                    <img src={selbstwert} alt="selbstwert" srcset="" />
+                </div>
+                <div class="service-text">
+                    <p>
+                        Wir alle sind viel zu oft von negativen Einflüssen
+                        umgeben, die sich nicht gut auf unser Selbst auswirken.
+                        Dabei wird unser Selbstwertgefühl angekratzt und
+                        beschädigt. Daher ist es wichtig, sich mit sich selbst
+                        auseinanderzusetzten, seine Gefühle, Gedanken und
+                        Handlungen einschätzen zu können, um die Möglichkeit zu
+                        haben eigenverantwortlich und selbstbewusst durch das
+                        Leben zu gehen. Des Weiteren wirkt der Selbstwert als
+                        ein wichtiger Schutzfaktor gegen Stress und ist somit
+                        ein positiver Faktor für unsere Widerstandskraft und
+                        Gesundheit.
+                        <br />
+                        Ich würde mich freuen, Sie kennenzulernen und Sie darin zu
+                        unterstützen, Ihren Selbstwert zu steigern und sich einfach
+                        wohlzufühlen.
+                    </p>
+                </div>
+            </div>
+            <div class="service digital-detox">
+                <h2>Digital Detox</h2>
+                <div class="service-image">
+                    <img src={digitaldetox} alt="digital-detox" srcset="" />
+                </div>
+                <div class="service-text">
+                    <p>
+                        Wir befinden uns in einem Zeitalter, welches durch
+                        Schnelllebigkeit und Hektik geprägt ist. Wir nehmen uns
+                        viel zu selten den Raum und die Zeit für uns selbst.
+                        Einfach mal entspannen und uns eine Auszeit vom Alltag
+                        nehmen, um unseren Körper und Geist in Einklang zu
+                        bringen. Hierfür bietet die Tiefenentspannung mittels
+                        Hypnose die perfekten Rahmenbedingungen. Ich würde mich
+                        freuen, Sie kennenzulernen und Sie auf dem Weg der
+                        Tiefenentspannung zu begleiten.
+                    </p>
+                </div>
+            </div>
+            <div class="service tiefenentspannugn">
+                <h2>Tiefenentspannung</h2>
+                <div class="service-image">
+                    <img
+                        src={tiefenentspannung}
+                        alt="tiefenentspannung"
+                        srcset=""
+                    />
+                </div>
+                <div class="service-text">
+                    <p>
+                        Wer kennt es nicht? Es steht eine Prüfung oder ein
+                        wichtiger Vortrag an und die Aufregung steigt in einem
+                        hoch, man bekommt schwitzige Hände oder der Herzschlag
+                        erhöht sich. Bei vielen Menschen führen solche
+                        Situationen zu einem kompletten Blackout, was Sie
+                        nachhaltig beschäftigt oder sogar belastet. Mittels
+                        Hypnose ist es möglich, sich diese Situationen genauer
+                        anzuschauen und aus einem anderen Blickwinkel zu
+                        betrachten und anzunehmen Ich möchte Sie darin zu
+                        unterstützen, entspanntere Vorträge zu halten oder
+                        Prüfungen zu meistern und zusätzlich Ihr
+                        Selbstbewusstsein zu stärken. Ich freue mich darauf Sie
+                        kennenzulernen.
+                    </p>
+                </div>
+            </div>
+            <div class="service pruefungsangst">
+                <h2>Entspannt Vortragen / Enspannt in die Prüfung</h2>
+                <div class="service-image">
+                    <img src={pruefungsangst} alt="pruefungsangst" srcset="" />
+                </div>
+                <div class="service-text">
+                    <p>
+                        Instagram, Snapchat, WhatsApp, TikTok, YouTube und
+                        vieles mehr sind unsere täglichen Begleiter, die jeden
+                        Menschen unterschiedlich beeinflussen. Wir meinen, dass
+                        dieses Thema nur Jugendliche betrifft, das ist aber
+                        falsch. Auch die Erwachsenen bleiben von der
+                        Informationsflut nicht verschont. Aber warum nutzen
+                        viele die Sozialen Medien in einem Übermaß oder bekommen
+                        ein ungutes Gefühl, wenn das Handy mal nicht in
+                        Reichweite ist? Eine mögliche Ursache könnte ein
+                        bestimmtes Hormon sein, welches bei der Handy-Nutzung
+                        aktiv wird. Dopamin ist ein Glückshormon, das im Körper
+                        ausgeschüttet wird, was dazu führt, dass wir uns gut
+                        fühlen und diesen Zustand gerne aufrechterhalten wollen.
+                        Wir haben das Bedürfnis nach mehr. Mit jedem Klingeln
+                        oder Vibrieren des Handys findet eine Art
+                        Konditionierung statt. Der Griff nach dem Handy und das
+                        Anklicken der jeweiligen Benachrichtigung führt zur
+                        Ausschüttung von Dopamin. Wir fühlen uns gut. Gerade in
+                        der heutigen Zeit ist es daher wichtig, sich die Frage
+                        zu stellen, warum wir die Sozialen Medien nutzen und wie
+                        viel wir davon brauchen? Es geht hierbei keinesfalls um
+                        die Verteufelung von Sozialen Medien, sondern vielmehr
+                        darum, sich selbst zu überlegen, wie möchte ich damit
+                        umgehen und eine annehmbare Einstellung dahingehend zu
+                        entwickeln.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style lang="scss">
+    .section {
+        position: relative;
+        min-height: 100vh;
+        height: 100%;
+        width: 100%;
+        min-width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        background-color: red;
+    }
+
+    .content-wrapper {
+        box-sizing: border-box;
+        display: flex;
+        position: relative;
+        flex-direction: column;
+        background: var(--bg-content-wrapper, transparent);
+        min-height: 100vh;
+        height: 100%;
+        width: 100%;
+        max-width: var(--content-width, 1240px);
+        // overflow: hidden;
+        // background-color: green;
+
+        padding: 100px 20px 0 20px;
+    }
+    .service-image {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        max-height: 350px;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        img {
+            object-position: 0% -20px;
+        }
+        margin-bottom: .5rem;
+    }
+    .service-text {
+        p{
+            font-family: var(--font-family, "Segoe UI");
+            font-size: var(--font-size-standard, 16px);
+            font-weight: var(--font-weight-standard, 300);
+        }
+    }
+    
+    .service{
+        height: auto;
+        width: 100%;
+        margin-bottom:2rem;
+    }
+    .section-one {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: var(--bg-section-one);
+        // overflow: hidden;
+        // min-height: 1000px;
+        .wrapper-services {
+            height: 100%;
+            width: 100%;
+            .service{
+                height: auto;
+                width: 100%;
+                .rauchfrei {
+                    height: auto;
+                    width: 100%;
+                    
+                }
+            }
+           
+           
+        }
+    }
+</style>
