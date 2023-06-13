f8f7093ef8c53279c9af8bcf21617d356132db5f
lauhard.andreas <lauhard.andreas@gmail.com>
2023-06-13 17:40:52 +0200

    fix heading

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    export let className="";
+    export let className = "";
 </script>
 
 <header class="heading {className}">
@@ -8,25 +8,26 @@
         <span class="line-2">für <span class="hypnose">Hypnose</span> &</span>
         <span class="line-3">Weiterentwicklung</span>
         <span class="subheading">Entdecken Sie die Kraft von Hypnose</span>
-        <div class="separator"></div>
+        <div class="separator" />
     </h1>
 </header>
 
 <style lang="scss">
-    span{
+    span {
         font-family: var(--font-family, "Segoe UI");
         font-style: normal;
         font-weight: 300;
-        font-size: 40px;
         font-size: 1.86rem;
-        line-height:  1.86rem;
+        line-height: 1.86rem;
         width: 100%;
-        letter-spacing: 2.9px;
-        letter-spacing: .13rem;
+        letter-spacing: 0.13rem;
         text-transform: uppercase;
         color: var(--text);
+        margin: 0;
+        padding: 0;
     }
-    .heading{
+    .heading {
+       
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -34,108 +35,82 @@
         width: auto;
         height: auto;
         background-color: #ffffff;
-        box-sizing:border-box;
+        box-sizing: border-box;
         white-space: nowrap;
-        border:5px solid white;
+        border: 5px solid white;
         // box-shadow: 0px 6px 15px -5px rgba(0, 0, 0, 0.25);
         width: 22.5rem;
     }
-    .wrapper{
+    .wrapper {
+      
         display: flex;
-        box-sizing:border-box;
+        box-sizing: border-box;
         flex-direction: column;
-        border:2px solid var(--secondary);
-        border:2px solid var(--primary);
-        padding: .9rem;
+        border: 2px solid var(--secondary);
+        border: 2px solid var(--primary);
+        padding: 0.9rem;
         width: 100%;
-        .line-1{
-            font-size: 15px;
-            font-size: .72rem;
+        .line-1 {
+            font-size: 0.72rem;
             line-height: 18px;
-            letter-spacing: 3.5px;
-            letter-spacing: .206rem;
+            letter-spacing: 0.206rem;
         }
-        .line-2{
-            .hypnose{
+        .line-2 {
+            .hypnose {
                 color: var(--text);
                 color: var(--secondary);
             }
         }
-        .subheading{
-            line-height: 30px;
-            font-size: .72rem;
-            letter-spacing: .206rem;
+        .subheading {
+            // line-height: 1.72rem;
+            // line-height:30px;
+            font-size: 0.72rem;
+            letter-spacing: 0.206rem;
         }
     }
     .separator {
+        margin: 0;
+        padding: 0;
         background-color: var(--secondary);
-        width:12.9rem;
+        width: 12.9rem;
         height: 3px;
         align-self: flex-start;
         border-radius: 5px;
     }
-    @media screen and (min-width: 319px) and (max-width: 1093px) {
-    //     span{
-    //     font-family: var(--font-family, "Segoe UI");
-    //     font-style: normal;
-    //     font-weight: 300;
-    //     font-size: 40px;
-    //     font-size: 1.85rem;
-    //     line-height:  1.85rem;
-    //     width: 100%;
-    //     letter-spacing: 2.9px;
-    //     letter-spacing: .13rem;
-    //     text-transform: uppercase;
-    //     color: var(--text);
-    // }
-    // .heading{
-    //     display: flex;
-    //     flex-direction: column;
-    //     justify-content: center;
-    //     align-items: center;
-    //     background-color: #ffffff;
-    //     box-sizing:border-box;
-    //     white-space: nowrap;
-    //     border:5px solid white;
-    //     // box-shadow: 0px 6px 15px -5px rgba(0, 0, 0, 0.25);
+    @media screen and (max-width:375px) {
+        .wrapper{
+            margin: 0;
+            width: auto;
 
-    // }
-    // .wrapper{
-    //     width: 360px;
-    //     display: flex;
-    //     box-sizing:border-box;
-    //     flex-direction: column;
-    //     border:2px solid var(--secondary);
-    //     border:2px solid var(--primary);
-    //     // padding: 20px;
-    //     .line-1{
-    //         font-size: 15px;
-    //         font-size: .70rem;
-    //         line-height: 18px;
-    //         letter-spacing: 3.5px;
-    //         letter-spacing: .18rem;
-    //     }
-    //     .line-2{
-    //         .hypnose{
-    //             color: var(--text);
-    //             color: var(--secondary);
-    //         }
-    //     }
-    //     .subheading{
-    //         line-height: 30px;
-    //         font-size: .70rem;
-    //         letter-spacing: .118rem;
-    //     }
-    // }
-    // .separator {
-    //     background-color: var(--secondary);
-    //     width:12.9rem;
-    //     height: 3px;
-    //     // align-self: flex-start;
-    //     // border-radius: 5px;
-    // }
+        }
+        span{
+            font-size: 26px;
+            line-height: auto;
+            width: auto;
+        }
+        .heading {
+            width: auto;
+            margin: 0px;
+            .line-1 {
+            font-size: auto;
+            // line-height: 18px;
+        }
+        .line-2 {
+            .hypnose {
+                color: var(--text);
+                color: var(--secondary);
+            }
+        }
+        .subheading {
+            line-height: 1.5rem;
+            // line-height:30px;
+            font-size: 0.65rem;
+            // font-size: 10px;
+            letter-spacing: 0.15rem;
+
+            // letter-spacing: 0.206rem;
+        }
+        }
     }
-
-
 
 </style>
