a87b993c7369c30ef147a23a52a3ce676f477c7b
lauhard.andreas <lauhard.andreas@gmail.com>
2023-07-06 15:05:02 +0200

    apple styling

--- src/lib/components/Information.svelte
+++ src/lib/components/Information.svelte
@@ -36,7 +36,7 @@
                 <span class="attention">Schlafprobleme</span>?
             </p>
         </div>
-        <a class="cta" type="button" style="background:red;"href="/leistungen"
+        <a class="cta" type="button" style="background:blue;"href="/leistungen"
             >HIER ERFAHREN SIE MEHR</a
         >
     </div>
