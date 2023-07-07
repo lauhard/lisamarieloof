1d04e6ea7125e194a4151d82bd51b9029c131ac7
lauhard.andreas <lauhard.andreas@gmail.com>
2023-07-08 00:43:39 +0200

    try fix build

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,16 +2,16 @@ import {createClient} from '$lib/prismicio'
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-// export const load: PageServerLoad = async (data) => {
-//    const {fetch, request, route} = data;
-//   const slug = route.id == '/' ? 'homepage' : route.id; 
-//     const homepageUID = slug  // Update for the UID of your homepage
-//     const client = createClient({ fetch, request })
-//     const document = await client.getFirst()
+export const load: PageServerLoad = async (data) => {
+   const {fetch, request, route} = data;
+  const slug = route.id == '/' ? 'homepage' : route.id; 
+    const homepageUID = slug  // Update for the UID of your homepage
+    const client = createClient({ fetch, request })
+    const document = await client.getFirst()
 
-//     if (document) {
-//         return { document }
-//     }
+    if (document) {
+        return { document }
+    }
 
-//     error(404, 'Not found')
-// };
+    error(404, 'Not found')
+};
